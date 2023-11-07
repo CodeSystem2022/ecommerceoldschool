7d14f1dd0d886978375cf6acea36225dba08692f
AndreaReynoso132 <95582384+AndreaReynoso132@users.noreply.github.com>
2023-11-07 01:23:30 -0300

    Firt commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+// PRODUCTOS
+const productos = [
+    // Iphone
+    {
+        id: "iphone-001",
+        titulo: "Iphone 01",
+        imagen: "./img/Iphone/001.jpg",
+        categoria: {
+            nombre: "Iphone",
+            id: "iphone"
+        },
+        precio: 1500000
+    },
+    {
+        id: "iphone-002",
+        titulo: "Iphone 02",
+        imagen: "./img/Iphone/002.jpg",
+        categoria: {
+            nombre: "Iphone",
+            id: "iphone"
+        },
+        precio: 1000000
+    },
+    {
+        id: "iphone-003",
+        titulo: "Iphone 03",
+        imagen: "./img/Iphone/003.jpg",
+        categoria: {
+            nombre: "Iphone",
+            id: "iphone"
+        },
+        precio: 900000
+    },
+    
+    // Motorola
+    {
+        id: "motorola-01",
+        titulo: "Motorola 01",
+        imagen: "./img/motorola/1.png",
+        categoria: {
+            nombre: "Motorola",
+            id: "motorola"
+        },
+        precio: 700000
+    },
+    {
+        id: "motorola-02",
+        titulo: "Motorola 02",
+        imagen: "./img/motorola/2.png",
+        categoria: {
+            nombre: "Motorola",
+            id: "motorola"
+        },
+        precio: 800000
+    },
+    {
+        id: "motorola-03",
+        titulo: "Motorola 03",
+        imagen: "./img/motorola/3.png",
+        categoria: {
+            nombre: "Motorola",
+            id: "motorola"
+        },
+        precio: 750000
+    },
+    
+    // Samsung
+    {
+        id: "samsung-01",
+        titulo: "Samsung 01",
+        imagen: "./img/samsung/01.png",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung"
+        },
+        precio: 65000
+    },
+    {
+        id: "samsung-02",
+        titulo: "Samsung 02",
+        imagen: "./img/samsung/02.png",
+        categoria: {
+            nombre: "Samsung",
+            id: "sumsung"
+        },
+        precio: 200000
+    },
+    {
+        id: "samsung-03",
+        titulo: "Samsung 03",
+        imagen: "./img/samsung/03.png",
+        categoria: {
+            nombre: "Samsung",
+            id: "samsung"
+        },
+        precio: 100000
+    },
+    
+];
